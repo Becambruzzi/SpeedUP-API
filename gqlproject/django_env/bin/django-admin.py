22f590c886e2102c698ba6eca77d279b57591e5d
Bernardo <bfacambruzzi@gmail.com>
2020-07-19 13:50:12 -0300

    heroku setup

---
+++ gqlproject/django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bernardocambruzzi/Desktop/gqlAPI/gqlproject/django_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- gqlproject/gqlproject/settings.py
+++ gqlproject/gqlproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'btw^g64-0v432+z*8xu5214jkvu&g#b!%^qu2lt&9rwx#g5(q-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
