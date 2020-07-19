182d9200c0164e49b3f13baebeb761f616b31951
Bernardo <bfacambruzzi@gmail.com>
2020-07-19 15:30:51 -0300

    fix procfile

--- gqlproject/django_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/bernardocambruzzi/Desktop/gqlAPI/gqlproject/django_env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
