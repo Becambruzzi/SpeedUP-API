81eaed6b1399cc9aeadffef59bb02c181db17563
Bernardo <bfacambruzzi@gmail.com>
2020-07-25 14:12:28 -0300

    additional files and changes for Heroku

--- gqlproject/gqlproject/settings.py
+++ gqlproject/gqlproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'btw^g64-0v432+z*8xu5214jkvu&g#b!%^qu2lt&9rwx#g5(q-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 ALLOWED_HOSTS = []
 
@@ -84,8 +87,8 @@ WSGI_APPLICATION = 'gqlproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gqlproject2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gqlproject',
         'USER': 'becambruzzi',
         'PASSWORD': '',
         'HOST': 'localhost',
@@ -131,4 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
